51d40441f4309003786b49f29c05f88b1196d407
Carston Wiebe <cwiebe3@huskers.unl.edu>
2023-08-23 22:12:51 -0500

    added major to output of Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,13 @@
+/**
+ * author: Carston Wiebe
+ * cwiebe3@huskers.unl.edu
+ * 
+ * date: 2023/08/23
+ * 
+ * Hello world code for Hack 1.0
+ */
 public class Hello {
     public static void main(String[] args) throws Exception {
-        System.out.println("Carston Wiebe");
+        System.out.println("Carston Wiebe\nComputer Engineering\n");
     }
 }
